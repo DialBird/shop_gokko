15a6a5ba832ccc03716884ee94d10bac0546af83
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-10 02:22:12 +0000

    annotateの実行とproductへのバリデーションの追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: products # 商品
+#
+#  id         :integer          not null, primary key
+#  name       :string           default(""), not null  # 商品名
+#  price      :integer          default("0"), not null # 価格
+#  cost       :integer          default("0"), not null # 原価
+#  image      :string                                  # 商品画像
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Product < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
+  validates :name, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cost, presence: true,
+                   numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products # 商品
+#
+#  id         :integer          not null, primary key
+#  name       :string           default(""), not null  # 商品名
+#  price      :integer          default("0"), not null # 価格
+#  cost       :integer          default("0"), not null # 原価
+#  image      :string                                  # 商品画像
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
