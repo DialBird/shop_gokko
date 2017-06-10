b98d158437eb350bfd11ec6f89b414a4f1561499
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-10 02:37:57 +0000

    seedにuserを追加する

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+# user
+User.create(email: 'rememberthatk@icloud.com', password: 'password')
+
+# product
 Product.create(name: 'りんご', price: 200, cost: 100, image: File.open("#{Rails.root}/db/data/apple.jpg"))
 Product.create(name: 'バナナ', price: 100, cost: 50, image: File.open("#{Rails.root}/db/data/banana.jpg"))
 Product.create(name: 'ぶどう', price: 500, cost: 20, image: File.open("#{Rails.root}/db/data/grape.jpg"))
