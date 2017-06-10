f5e2466006e640c7796367b0b6e748cb64821eab
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-10 03:03:55 +0000

    商品一覧画面を作成する

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+
+  resources :products
 end
