db4ddc32951635cecc9761626d03f3b1eae980d9
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-10 12:33:55 +0000

    cartの詳細画面（cart#edit）を作成する

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,4 @@
+class CartController < ApplicationController
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
   resources :products
+
+  get '/cart' => 'cart#edit'
 end
