52dcc64022b89ef771e1b16424d6ce9d3b9a58c2
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-10 12:17:12 +0000

    cart_itemのquantityの最小値を0にする

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -15,5 +15,5 @@ class CartItem < ApplicationRecord
   belongs_to :cart
 
   validates :quantity, presence: true,
-                       numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+                       numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
