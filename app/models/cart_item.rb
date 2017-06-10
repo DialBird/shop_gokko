d454c9ca747f5ccef6f2babd2516086ce540bd3d
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-10 10:23:13 +0000

    カート商品にバリデーションを追加する

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -13,4 +13,7 @@
 class CartItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+
+  validates :quantity, presence: true,
+                       numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
