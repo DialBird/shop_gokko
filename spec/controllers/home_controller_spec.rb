2b298adf2a2d3bc28541ce989e8af3be36186c03
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-11 01:17:45 +0000

    homeとproductsのコントローラーのRSpecを追加する

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  login_user
 
   describe "GET #index" do
-    it "returns http success" do
+    it 'ビューを表示すること' do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response.status).to eq 200
+      expect(response).to render_template :index
     end
   end
-
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,10 +3,20 @@ require 'rails_helper'
 RSpec.describe ProductsController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    login_user
+
+    before { FactoryGirl.create_list(:product, 3) }
+
+    it 'ビューを表示すること' do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response.status).to eq 200
+      expect(response).to render_template :index
+    end
+
+    it 'インスタンス変数が格納されていること' do
+      get :index
+      expect(assigns[:products]).to be_present
+      expect(assigns[:products].size).to eq 3
     end
   end
-
 end
