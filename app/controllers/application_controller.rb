1388dd46157399e345d6a09c615f03119d90f340
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-12 12:50:45 +0000

    state machineの不要な起動を回避し、ログインしていない間の臨時的な処理をする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,18 +6,20 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
 
   def current_cart(options = {})
+    # guest tokenを作るまではこれで我慢
+    return unless user_signed_in?
     # 必要か？
     return @current_cart if @current_cart
 
     @current_cart = Cart.incomplete.find_by(user_id: current_user.id)
     options[:create_cart_if_necessary] ||= false
     if @current_cart.blank? and options[:create_cart_if_necessary]
-      @current_cart = Cart.create(user_id: current_user.id, state: "address")
+      @current_cart = Cart.create(user_id: current_user.id)
     end
     @current_cart
   end
 
   def back_to_address
-    current_cart.try!(:back)
+    current_cart.try!(:back) if current_cart.can_back?
   end
 end
